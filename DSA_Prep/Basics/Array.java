0e5d7b34f38120157870ba16719f10ca35c19562
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-01-14 21:16:54 +0530

    Array

---
+++ DSA_Prep/Basics/Array.java
@@ -0,0 +1,44 @@
+public class Array {
+    public static void main(String[] args) {
+        // dataType[] arrayName; e.g. int[] arr;
+
+        // declare an array
+        double[] arr;
+
+        // allocate memory
+        arr = new double[10];
+
+        // Alternatively, we can declare and allocate memory in one step
+
+        double[] arr2 = new double[10];
+
+
+        // declare and initialize an array
+        int[] age = {12, 4, 5, 2, 5};
+
+        // declare and array 
+        int[] arr3 = new int[10];
+
+        // initialize array
+        age[0] = 12;
+        age[1] = 4;
+        age[2] = 5;
+        age[3] = 2;
+
+
+        // using loop to access array elements
+        for (int i = 0; i < age.length; i++) {
+            System.out.println(age[i]);
+        }
+
+        // using for-each loop to access array elements
+        for (int i : age) {
+            System.out.println(i);
+        }
+
+        
+
+
+
+    }
+}
