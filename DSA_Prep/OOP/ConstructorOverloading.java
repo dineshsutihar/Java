34c7210d7fdf6ea37873a2f049b14ef6a5472777
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-01-18 13:34:19 +0530

    constructor overloading

---
+++ DSA_Prep/OOP/ConstructorOverloading.java
@@ -0,0 +1,35 @@
+package DSA_Prep.OOP;
+
+public class ConstructorOverloading {
+
+    // Constructor cannot be abstract, final, and static.
+
+
+    String language; 
+
+    // constructor with no parameter
+    ConstructorOverloading(){
+        this.language = "Java";
+    }
+
+    // constructor with a single parameter
+    ConstructorOverloading(String language){
+        this.language = language;
+    }
+
+    public void getName(){
+        System.out.println("Programming Language: " + this.language);
+    }
+
+    public static void main(String[] args) {
+        
+        // call constructor with no parameter
+        ConstructorOverloading obj1 = new ConstructorOverloading();
+
+        // call constructor with a single parameter
+        ConstructorOverloading obj2 = new ConstructorOverloading("Python");
+
+        obj1.getName();
+        obj2.getName();
+    }
+}
