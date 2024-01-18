2eac8bd1469d0ac298b3e014c8726be8a3ccea6c
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-01-18 10:30:30 +0530

    added detailes of input and output

--- DSA_Prep/Basics/InputOutput.java
+++ DSA_Prep/Basics/InputOutput.java
@@ -10,10 +10,27 @@ public class InputOutput {
         int a = input.nextInt();
         System.out.println("You entered: " + a);
 
-        
+
         System.out.print("Enter a string: ");
-        String b = input.next();
+        String b = input.next(); // nextLine() for whole line and next() for single word
         System.out.printf("You entered: %s",b);
+
+
+        // Double input
+        System.out.print("Enter a double: ");
+        double c = input.nextDouble();
+        System.out.printf("You entered: %f",c);
+
+        // float input
+        System.out.print("Enter a float: ");
+        float d = input.nextFloat();
+        System.out.printf("You entered: %f",d);
+
+        // char input
+        System.out.print("Enter a char: ");
+        char e = input.next().charAt(0);
+        System.out.printf("You entered: %c",e);
+        
 
 
         input.close();
