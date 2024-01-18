92bcd899a17fd1483c1c023de3307b630511c590
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-01-18 10:20:53 +0530

    turnary operator

---
+++ DSA_Prep/Basics/TurnaryOperator.java
@@ -0,0 +1,8 @@
+public class TurnaryOperator {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        String result= (a>b) ? "a is greater than b" : "b is greater than a";
+        System.out.println(result);
+    }
+}
