be5b1af8af979f40be9f95514c1bbc0bc2b30769
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-01-18 12:23:06 +0530

    Method and Method Overloading concept

---
+++ DSA_Prep/OOP/MethodOverloading.java
@@ -0,0 +1,55 @@
+package DSA_Prep.OOP;
+
+public class MethodOverloading {
+
+    // Method Overloading is a feature that allows a class to have more than one method having the same name, if their argument lists are different.
+
+    // it is just related to parameter list, not return type.
+
+    // Method Overloading is a compile-time polymorphism.
+
+    private static int add(int a, int b){
+        return a + b;
+    }
+
+    private static int add(int a, int b, int c){
+        return a + b + c;
+    }
+
+    private static double add(double a, double b){
+        return a + b;
+    }
+
+     // this method accepts int
+     private static void display(int a){
+        System.out.println("Got Integer data.");
+    }
+
+    // this method  accepts String object
+    private static void display(String a){
+        System.out.println("Got String object.");
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(add(10, 20));
+        System.out.println(add(10, 20, 30));
+        System.out.println(add(10.5, 20.5));
+
+        display(1);
+        display("Hello");
+
+    }
+
+    // Note:
+    /*
+        1. Two or more methods can have the same name inside   the same class if they accept different arguments. This feature is known as method overloading.
+        2. Method overloading is achieved by either:
+            i. changing the number of arguments.
+            ii. or changing the data type of arguments.
+        3. It is not method overloading if we only change the return type of methods. There must be differences in the number of parameters.
+     */
+
+
+    
+}
---
+++ DSA_Prep/OOP/Methods.java
@@ -0,0 +1,34 @@
+package DSA_Prep.OOP;
+
+public class Methods {
+    // A method is a block of code which only runs when it is called.
+
+    // create a method
+    public int addNumber(int a, int b){
+        return a + b;
+    }
+
+    // Create a static method which can be used without creating an object of the class.
+
+    public static int subtractNumber(int a, int b){
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        
+        int num1 = 10;
+        int num2 = 20;
+
+        // create an object of Methods
+        Methods obj = new Methods();
+
+        // call the addNumber() method
+        int result = obj.addNumber(num1, num2);
+        System.out.println("Sum is: " + result);
+
+        // call the subtractNumber() method
+        int result2 = subtractNumber(num1, num2);
+        System.out.println("Difference is: " + result2);
+
+    }
+}
